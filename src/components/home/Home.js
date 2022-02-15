0059704487a2a7e53b6b295cf995b3ea87c8bde0
N00b13-M1 <michael.cornelis@gmail.com>
2022-02-15 16:33:08 +0100

    NewCommit

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './home.sass';
 import Header from "../header/Header.js";
 import Footer from "../footer/Footer.js";
+import Contact from '../contact/Contact.js';
 
 
 function Home() {  
     return (
         <div>
             <Header />
+            <Contact/>
             <Footer />
         </div>
     )
