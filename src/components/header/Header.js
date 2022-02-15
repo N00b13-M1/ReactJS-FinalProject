c0f5ca1a8b33e7bfa11573891da6e0b85a21eb34
Isma <ismarehailia@gmail.com>
2022-02-15 13:44:33 +0100

    header in progress

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -6,7 +6,8 @@ function Header() {
     
     return (
         <div>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            {/* NavBar */}
+            <nav className="navbar navbar-expand-lg navbar-light">
                 <div className="container-fluid">
                 <a className="navbar-brand" href="#"><img src="./img/logo.webp" alt="Fashe." /></a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -14,34 +15,55 @@ function Header() {
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li className="nav-item">
-                        <a className="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Link</a>
-                    </li>
-                    <li className="nav-item dropdown">
-                        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Dropdown
-                        </a>
-                        <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <li><a className="dropdown-item" href="#">Action</a></li>
-                        <li><a className="dropdown-item" href="#">Another action</a></li>
-                        <li><hr className="dropdown-divider" /></li>
-                        <li><a className="dropdown-item" href="#">Something else here</a></li>
-                        </ul>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link disabled" href="#" tabIndex={-1} aria-disabled="true">Disabled</a>
-                    </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Product</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">About</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Contact</a>
+                        </li>
                     </ul>
-                    <form className="d-flex">
-                    <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                    <button className="btn btn-outline-success" type="submit">Search</button>
-                    </form>
+                    <ul className="d-flex">
+                        <li className="nav-item">
+                            <a className="nav-link" href="#"><img src="./img/icon-header-01.png.webp" alt="basket" /></a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#"><img src="./img/icon-header-02.png.webp" alt="basket" /></a>
+                        </li>
+                    </ul>
                 </div>
                 </div>
             </nav>
+            {/* Header - Carousel */}
+            <div className="title">
+                <h1></h1>
+            </div>
+            <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
+                <div className="carousel-inner">
+                    <div className="carousel-item active">
+                        <img src="./img/master-slide-04.jpg.webp" className="d-block w-100" alt="Master Slide 01" />
+                    </div>
+                    <div className="carousel-item">
+                        <img src="./img/master-slide-03.jpg.webp" className="d-block w-100" alt="Master Slide 02" />
+                    </div>
+                    <div className="carousel-item">
+                        <img src="./img/master-slide-02.jpg.webp" className="d-block w-100" alt="..." />
+                    </div>
+                </div>
+                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true" />
+                    <span className="visually-hidden">Previous</span>
+                </button>
+                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true" />
+                    <span className="visually-hidden">Next</span>
+                </button>
+            </div>
         </div>
     )
 }
