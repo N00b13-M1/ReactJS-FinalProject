fb3340f085b14c3a8377c1cb58c1bd144f7ef2a3
N00b13-M1 <michael.cornelis@gmail.com>
2022-02-15 11:49:34 +0100

    Removed-bg-blue

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./header.sass";
-import {logo} from "../../img/logo.png.webp";
+
 
 function Header() {
     
@@ -8,7 +8,7 @@ function Header() {
         <div>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <div className="container-fluid">
-                <a className="navbar-brand" href="#"><img src={logo} alt="Fashe." /></a>
+                <a className="navbar-brand" href="#"><img src="./img/logo.webp" alt="Fashe." /></a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon" />
                 </button>
