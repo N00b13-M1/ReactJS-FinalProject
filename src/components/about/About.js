b82fb186d789ff661a114cefd45c5e74f30dee44
Isma <ismarehailia@gmail.com>
2022-02-16 11:48:32 +0100

    sauvegarde michael

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,6 +1,5 @@
 import React from "react";
-import HeroBanner from "../hero-banner/HeroBanner.js";
-import OurStory from '../about/OurStory.js';
+import OurStory from './OurStory.js';
 import "./about.sass";
 
 export default function About() {
