d933ccc3c7f93658abb7318a1a01cd373c16a7cf
N00b13-M1 <michael.cornelis@gmail.com>
2022-02-16 09:55:33 +0100

    MoveComponentsToApp

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import './home.sass';
-import Navigation from "../navigation/Navigation.js"
 import HeroBanner from "../hero-banner/HeroBanner.js";
-import Footer from "../footer/Footer.js";
 import OurProduct from "../home/OurProduct.js";
 import OurStory from '../about/OurStory.js';
 import Banner from '../banner/Banner.js'
@@ -18,7 +16,6 @@ function Home() {
     }  
     return (
         <div>
-            <Navigation />
             <HeroBanner/>
             <OurProduct />
             <OurStory/>
@@ -29,7 +26,6 @@ function Home() {
                     )
                 })
             }
-            <Footer />
         </div>
     )
 }
