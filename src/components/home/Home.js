d9123c4698a7b49555a1c748d98985c4ad98a007
Isma <ismarehailia@gmail.com>
2022-02-16 09:51:55 +0100

    save

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -4,30 +4,29 @@ import Navigation from "../navigation/Navigation.js"
 import HeroBanner from "../hero-banner/HeroBanner.js";
 import Footer from "../footer/Footer.js";
 import OurProduct from "../home/OurProduct.js";
-import OurStory from '../about/OurStory.js';
-import Banner from '../banner/Banner.js'
+
 
 function Home() {
     
-    const array = {
-        titles: [
-        "ABOUT",
-        "COUP DE COEUR",
-        "CONTACT"
-        ]
-    }  
+    // const array = {
+    //     titles: [
+    //     "ABOUT",
+    //     "COUP DE COEUR",
+    //     "CONTACT"
+    //     ]
+    //}  
     return (
         <div>
             <Navigation />
             <HeroBanner/>
             <OurProduct />
-            {
+            {/* {
                 array.titles.map((element,index) => {
                     return(
                         <Banner key={index} title={element}/>
                     )
                 })
-            }
+            } */}
             <Footer />
         </div>
     )
