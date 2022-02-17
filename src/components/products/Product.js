b019375c209c42df48f741859607e75c6b602dc1
Isma <ismarehailia@gmail.com>
2022-02-17 22:18:22 +0100

    product list filter (sale)

--- src/components/products/Product.js
+++ src/components/products/Product.js
@@ -29,23 +29,45 @@ export default function Product(props) {
                             <li className='my-1' onClick={() => {changepageCategory('women')}}>Women</li>
                             <li className='my-1' onClick={() => {changepageCategory('kids')}}>Kids</li>
                             <li className='my-1' onClick={() => {changepageCategory('men')}}>Men</li>
-                            <li className='my-1' onClick={() => {changepageCategory('accessories')}}>Accessories</li>
+                            <li className='my-1 mb-2' onClick={() => {changepageCategory('accessories')}}>Accessories</li>
                             <h4>Filter</h4>
                             <input type="text" placeholder='Search Product' />
+                            
                         </ul>
                     </div>
                     {/* Products */}
                     <div className='col-md-9 col-12 row justify-content-center p-4 pt-0'>
-                        <ul className="row justify-content-center p-0">
+                        <ul className="row p-0">
                             {
                                 itemList.filter(chaqueElementDuTableau => chaqueElementDuTableau.category.toLowerCase().includes(pageCategory))
                                 .map((element,index) =>{
                                     return(
-                                        <li className='col-md-3 col-12 mb-3 mx-md-3 p-0' key={index}>
-                                            <img className="product-item-cover col-12" src={element.cover} alt=""/>
-                                            <p className='mb-0 mt-2 text-dark'>{element.thename}</p>
-                                            <p>{element.price}</p>
-                                        </li>
+                                        <>
+                                            {
+                                                element.sale === true &&
+                                                <li className='col-md-3 col-12 mb-3 mx-md-3 p-0' key={index}>
+                                                    <p className='sale py-1 px-2 ms-2 mt-2'>Sale</p>
+                                                    <img className="product-item-cover col-12" src={element.cover} alt=""/>
+                                                    <p className='mb-0 mt-2 text-dark'>{element.thename}</p>
+                                                    <p>{element.price}</p>
+                                                </li>
+                                            }
+                                            {
+                                                element.sale === false &&
+                                                <li className='col-md-3 col-12 mb-3 mx-md-3 p-0' key={index}>
+                                                    <img className="product-item-cover col-12" src={element.cover} alt=""/>
+                                                    <p className='mb-0 mt-2 text-dark'>{element.thename}</p>
+                                                    <p>{element.price}</p>
+                                                </li>
+                                            }
+                                            
+
+                                            {/* <li className='col-md-3 col-12 mb-3 mx-md-3 p-0' key={index}>
+                                                <img className="product-item-cover col-12" src={element.cover} alt=""/>
+                                                <p className='mb-0 mt-2 text-dark'>{element.thename}</p>
+                                                <p>{element.price}</p>
+                                            </li> */}
+                                        </>
                                     )
                                 })
                             }
