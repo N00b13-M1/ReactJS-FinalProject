67a0f3bbfedb6b38853d960fb51b1f5997b990da
Isma <ismarehailia@gmail.com>
2022-02-17 19:58:16 +0100

    coeur & panier added to router

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,8 @@ import Home from "./home/Home.js";
 import Product from "./products/Product.js";
 import About from "./about/About.js";
 import Contact from "./contact/Contact.js";
+import Coeur from "./coeur/Coeur.js";
+import Panier from "./panier/Panier.js";
 import Footer from "./footer/Footer.js";
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
@@ -19,6 +21,8 @@ export default function App() {
             <Route path={"/product"} element={<Product/>} />
             <Route path={"/about"} element={<About/>} />
             <Route path={"/contact"} element={<Contact/>} />
+            <Route path={"/coeur"} element={<Coeur/>} />
+            <Route path={"/panier"} element={<Panier/>} />
           </Routes>
           <Footer />
         </BrowserRouter>
--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -9,32 +9,32 @@ export default function Navigation() {
             {/* NavBar */}
             <nav className="navbar navbar-expand-lg navbar-light">
                 <div className="container-fluid">
-                <a className="navbar-brand" href="###"><img src="./img/logo.webp" alt="Fashe." /></a>
+                <Link className="nav-link" to="/"><img src="./img/logo.webp" alt="Fashe." /></Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon" />
                 </button>
                 <div className="collapse navbar-collapse row" id="navbarSupportedContent">
                     <div className="row">
-                            <ul className="navbar-nav me-auto mb-2 mb-lg-0 col-11 justify-content-center align-items-center">
-                                <li className="nav-item">
-                                    <Link className="nav-link" to="/">Home</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link className="nav-link" to="/product">Product</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link className="nav-link" to="/about">About</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link className="nav-link" to="/contact">Contact</Link>
-                                </li>
-                            </ul>
-                        <ul className="d-flex p-0 col-1">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0 col-11 justify-content-center align-items-center">
                             <li className="nav-item">
-                                <a className="nav-link" href="###"><img src="./img/icon-header-01.png.webp" alt="basket" /></a>
+                                <Link className="nav-link" to="/">Home</Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="###"><img src="./img/icon-header-02.png.webp" alt="basket" /></a>
+                                <Link className="nav-link" to="/product">Product</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/about">About</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/contact">Contact</Link>
+                            </li>
+                        </ul>
+                        <ul className="d-flex p-0 col-1">
+                            <li className="nav-item">
+                                <a className="nav-link" href="#"><i className="fa-regular fa-heart" /></a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#"><i className="fa-regular fa-heart" /></a>
                             </li>
                         </ul>
                     </div>
