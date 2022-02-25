91d37bea28b0446d3fb361d87f4b6c4d4fb73b5f
N00b13-M1 <michael.cornelis@gmail.com>
2022-02-25 21:09:22 +0100

    Newcommit

--- src/components/products/Product.js
+++ src/components/products/Product.js
@@ -9,82 +9,24 @@ export default function Product(props) {
 
 	const [pageCategory, setPageCategory] = useState('');
 
-    const changePageCategory = (newPageCategory) => {
-        setPageCategory(newPageCategory)
-        console.log(pageCategory);
-        
-    }   
+	const changepageCategory = (newPageCategory) => {
+		setPageCategory(newPageCategory)
+		console.log(pageCategory);
+	}
 
 
-    //Attempt for keywords search
-    let changeToCategory = (e) => {
-        let keyword = e.target.value
-        itemList.forEach(element => {
-            if (keyword === element.thename){
-                itemList.filter.category
-            }
-            
-        });
-        // itemList.forEach(element.thename => { 
-        //     keywords.push(element.thename)
-    }
+	//Attempt for keywords search
+	let changeToCategory = (e) => {
+			// let keywords = []
+			// itemList.forEach(element.thename => {
+			// 	keywords.push(element.thename)
+			// })
+			// if (e.key === "Enter") {
+			// 	console.log(Object.values(keywords))
+			// }
+	}
 
 
-
-
-
-
-
-
-    return (
-        <div className='text-center'>
-            <div className="backgroundbanner d-flex justify-content-center align-items-center">
-                <h2 className="text-white fs-1">PRODUCTS</h2>
-            </div>
-            <div className="container py-5">
-                <div className='row justify-content-center'>
-                    {/* Categories */}
-                    <div className='col-md-3 col-12 text-start'>
-                        <ul className='text-start'>
-                            <h4>Category</h4>
-                            <li className='my-1' onClick={() => {changePageCategory('')}}>All</li>
-                            <li className='my-1' onClick={() => {changePageCategory('Women')}}>Women</li>
-                            <li className='my-1' onClick={() => {changePageCategory('Kids')}}>Kids</li>
-                            <li className='my-1' onClick={() => {changePageCategory('Men')}}>Men</li>
-                            <li className='my-1 mb-2' onClick={() => {changePageCategory('Accessories')}}>Accessories</li>
-                            <h4>Filter</h4>
-                            <input type="text" placeholder='Search Product' onInput={(e) => changeToCategory(e)}/>
-                        </ul>
-                    </div>
-                    {/* Products */}
-                    <div className='col-md-9 col-12 row justify-content-center p-4 pt-0'>
-                    <ul className="row p-0">
-                            {
-                                itemList.filter(chaqueElementDuTableau => chaqueElementDuTableau.category.match(pageCategory)).map((element,index) =>{
-                                    return(
-                                        <>
-                                            {console.log(element)}
-                                            <li key={index} className='product-item col-md-3 col-12 mb-3 mx-md-3 p-0'>
-                                                {
-                                                    element.sale === true &&
-                                                    <p className='sale py-1 px-2 m-2'>Sale</p>
-                                                }
-                                                {
-                                                    element.new === true &&
-                                                    <p className='new py-1 px-2 m-2'>New</p>
-                                                }
-                                                {
-                                                    element.bestseller === true &&
-                                                    <p className='best-seller py-1 px-2 m-2'>Best 							Seller</p>
-                                                }
-                                                {
-                                                    element.old === true &&
-                                                    <p className='old py-1 py-1 px-2 m-2'>Old</p>
-                                                }
-                                                <img onClick={() => {props.addProductLikesProp(element)}} className="img-fluid" src={element.cover} alt="" />
-                                                <p className='mb-0 mt-2 text-dark fw-bold'>{element.thename}</p>
-                                                <p>{element.price}</p>
-                                            </li>
 
 	return (
 		<div className='text-center'>
